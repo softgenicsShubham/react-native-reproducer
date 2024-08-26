7293e49e6342479a385e96ffe5e58164e65006f6
Shubham ghanghotia <Softgenics.shubham@gmail.com>
2024-08-27 00:15:44 +0530

    new issue

--- App.tsx
+++ App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import FirstScreen from './src/screens/FirstScreen';
-import IndexScreen from './src/screens/IndexScreen';
-import { setHidden } from './src/native_modules/NavigationColorModules';
+// import React from 'react';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { NavigationContainer } from '@react-navigation/native';
+// import FirstScreen from './src/screens/FirstScreen';
+// import IndexScreen from './src/screens/IndexScreen';
+// import { setHidden } from './src/native_modules/NavigationColorModules';
 
-const Stack = createNativeStackNavigator();
-const App: React.FC = () => {
+// const Stack = createNativeStackNavigator();
+// const App: React.FC = () => {
+//   return (
+//     <NavigationContainer onReady={() => { setHidden(true); }}>
+//       <Stack.Navigator initialRouteName="IndexScreen" screenOptions={{ headerShown: false }}>
+//         <Stack.Screen name="FirstScreen" component={FirstScreen} />
+//         <Stack.Screen name="IndexScreen" component={IndexScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
+
+
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const { width, height } = Dimensions.get('screen')
+
+const App = () => {
   return (
-    <NavigationContainer onReady={() => { setHidden(true); }}>
-      <Stack.Navigator initialRouteName="IndexScreen" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="FirstScreen" component={FirstScreen} />
-        <Stack.Screen name="IndexScreen" component={IndexScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+    <View style={{ width, height }}>
 
-export default App;
+      <Text
+        style={{
+          position: 'absolute',
+          bottom: 40
+        }}>
+        Another text
+      </Text>
 
+      <Text
+        style={{
+          position: 'absolute',
+          bottom: 0
+        }}>
+        App
+      </Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
 
 
 
